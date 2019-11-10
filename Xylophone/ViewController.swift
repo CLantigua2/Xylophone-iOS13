fa1f70b967929d68198c5093e958f376dcf5782d
Carlos Lantigua <clantigua954@gmail.com>
2019-11-09 23:26:29 -0500

    completed xylophone project and connected wav file sound data to corresponding buttons using AVFoundation module

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -1,21 +1,39 @@
-//
-//  ViewController.swift
-//  Xylophone
-//
-//  Created by Angela Yu on 28/06/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
-
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    var player: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
     
-
+    @IBAction func keyPressedC(_ sender: UIButton) {
+        playSound(title: "C")
+    }
+    
+    @IBAction func keyPressedD(_ sender: UIButton) {
+        playSound(title: "D")
+    }
+    @IBAction func keyPressedE(_ sender: UIButton) {
+        playSound(title: "E")
+    }
+    @IBAction func keyPressedF(_ sender: UIButton) {
+        playSound(title: "F")
+    }
+    @IBAction func keyPressedG(_ sender: UIButton) {
+        playSound(title: "G")
+    }
+    @IBAction func keyPressedA(_ sender: UIButton) {
+        playSound(title: "A")
+    }
+    @IBAction func keyPressedB(_ sender: UIButton) {
+        playSound(title: "B")
+    }
+    
+    func playSound(title: String) {
+        let url = Bundle.main.url(forResource: title, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
 }
-
